5bb8536ac66e4d65d938fb107ff2e5193234cdd6
Den Fadeev <d3nf4d33v@gmail.com>
2023-07-26 22:22:18 +0300

    Реализованы эндпоинты USERS
    Работают все ендпониты users, в том числе пагинация,
    поиск по username, доступ пока неограничен.

---
+++ api_yamdb/users/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+
+from users.models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = (
+            'username',
+            'email',
+            'first_name',
+            'last_name',
+            'bio',
+            'role'
+        )
